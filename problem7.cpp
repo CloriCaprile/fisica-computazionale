9f6201827a349754a76ef243b6d1ce1daac67acc
diegoscantam <diego.scantamburlo@studenti.unitn.it>
2022-09-08 11:54:57 +0200

    Corrected indeces in error vectors

--- problem7.cpp
+++ problem7.cpp
@@ -32,11 +32,11 @@ int main(){
 
 
     // Set a filename 
-    //std::string filename = "u.txt";
+    std::string filename0 = "data/u"+ std::to_string(n)+ ".txt";
     // Create and open the output file. Or, technically, create 
     // an "output file stream" (type std::ofstream) and connect it to our filename.
-    //std::ofstream ofile;
-    //ofile.open(filename);
+    std::ofstream ofile0;
+    ofile0.open(filename0);
 
     // fill x and u vectors, (print them), and store them in a file "u.txt" 
     x[0]=x_min;
@@ -56,8 +56,10 @@ int main(){
         }
         // std::cout<< i << "," << f[i] << "," << g[i] << "\n";
         //std::cout << i << scientific_format(x[i], width, prec ) << " , " << scientific_format(u[i], width, prec )<<  "\n";
-        //ofile << scientific_format(x[i], width, prec ) << scientific_format(u[i], width, prec )<<  std::endl;
+        ofile0 << scientific_format(x[i], width, prec ) << scientific_format(u[i], width, prec )<<  std::endl;
     }
+    ofile0.close();
+
 
     // declare v and fill it with thomas algorithm by calling the function
     std::vector<double> v(n-1);
@@ -72,9 +74,9 @@ int main(){
 
     
     // Set some filenames
-    std::string filename = "v1000.txt";
-    std::string filename2 = "error.txt";
-    std::string filename3 = "rel_error.txt";
+    std::string filename = "data/v"+ std::to_string(n)+ ".txt";
+    std::string filename2 = "data/error"+ std::to_string(n) +".txt";
+    std::string filename3 = "data/rel_error"+ std::to_string(n) +".txt";
 
 
     // Create and open the output files
@@ -88,13 +90,13 @@ int main(){
 
     // save in file v(x)
     for(int i=0; i<=n; i++){
-    //ofile << scientific_format(x[i], width, prec ) << scientific_format(v_star[i], width, prec )<<  std::endl;
+    ofile << scientific_format(x[i], width, prec ) << scientific_format(v_star[i], width, prec )<<  std::endl;
     }
 
     // save in file error and relative error
     for(int i=0; i<=n-2; i++){
-    ofile2 << scientific_format(x[i], width, prec ) << scientific_format( abs(u[i+1]-v[i]), width, prec )<<  std::endl;
-    ofile3 << scientific_format(x[i], width, prec ) << scientific_format( abs((u[i+1]-v[i])/u[i+1]), width, prec )<<  std::endl;
+    ofile2 << scientific_format(x[i+1], width, prec ) << scientific_format( fabs((long double) u[i+1]-v[i]), width, prec )<<  std::endl;
+    ofile3 << scientific_format(x[i+1], width, prec ) << scientific_format( fabs(((long double) u[i+1]-v[i])/u[i+1]), width, prec )<<  std::endl;
     }
     // Close the output file
     ofile.close();
